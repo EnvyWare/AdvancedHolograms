71adb10b4e619d58660af1eb2bf43f0307b4ab1c
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-14 21:50:18 +0100

    Feat: added HologramBuilder

---
+++ api/src/main/java/com/envyful/holograms/api/HologramBuilder.java
@@ -0,0 +1,56 @@
+package com.envyful.holograms.api;
+
+/**
+ *
+ * Interface representing all builders for the {@link Hologram} interface
+ *
+ */
+public interface HologramBuilder {
+
+    /**
+     *
+     * Sets the world stored in the builder
+     *
+     * @param worldName The name of the world
+     * @return The builder
+     */
+    HologramBuilder world(String worldName);
+
+    /**
+     *
+     * Sets the position stored in the builder
+     *
+     * @param x The x position stored
+     * @param y The y position stored
+     * @param z The z position stored
+     * @return The builder
+     */
+    HologramBuilder position(double x, double y, double z);
+
+    /**
+     *
+     * Adds a single line to the hologram
+     *
+     * @param line The line to add
+     * @return The builder
+     */
+    HologramBuilder line(String line);
+
+    /**
+     *
+     * Adds multiple lines to the hologram
+     *
+     * @param lines The lines to add
+     * @return The builder
+     */
+    HologramBuilder lines(String... lines);
+
+    /**
+     *
+     * Builds the hologram from the specified values
+     *
+     * @return The hologram created
+     */
+    Hologram build();
+
+}
