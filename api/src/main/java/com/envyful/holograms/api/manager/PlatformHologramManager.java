c9ecae52a0d3fa323568b9d5dd5de887dd43421e
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-14 21:53:33 +0100

    Feat: added PlatformHologramManager.java

---
+++ api/src/main/java/com/envyful/holograms/api/manager/PlatformHologramManager.java
@@ -0,0 +1,36 @@
+package com.envyful.holograms.api.manager;
+
+import com.envyful.holograms.api.hologram.HologramBuilder;
+
+/**
+ *
+ * Interface implementation for creating instances of API interfaces
+ *
+ */
+public interface PlatformHologramManager {
+
+    /**
+     *
+     * Creates an empty builder
+     *
+     * @return The new builder
+     */
+    HologramBuilder builder();
+
+    /**
+     *
+     * Creates an empty builder with given lines
+     *
+     * @return The new builder
+     */
+    HologramBuilder builder(String... lines);
+
+    /**
+     *
+     * Creates an empty builder at given pos
+     *
+     * @return The new builder
+     */
+    HologramBuilder builder(String world, int x, int y, int z);
+
+}
