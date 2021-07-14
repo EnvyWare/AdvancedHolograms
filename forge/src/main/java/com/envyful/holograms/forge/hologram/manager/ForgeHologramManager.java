bffb129deda775f52fe90f6fed2cd8873a602934
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-15 00:17:26 +0100

    Feat: implemented ForgeHologramManager.java

---
+++ forge/src/main/java/com/envyful/holograms/forge/hologram/manager/ForgeHologramManager.java
@@ -0,0 +1,32 @@
+package com.envyful.holograms.forge.hologram.manager;
+
+import com.envyful.holograms.api.hologram.HologramBuilder;
+import com.envyful.holograms.api.manager.PlatformHologramManager;
+import com.envyful.holograms.forge.hologram.ForgeHologramBuilder;
+
+/**
+ *
+ * The forge implementation of the {@link PlatformHologramManager} interface
+ *
+ */
+public class ForgeHologramManager implements PlatformHologramManager {
+
+    @Override
+    public HologramBuilder builder() {
+        return new ForgeHologramBuilder();
+    }
+
+    @Override
+    public HologramBuilder builder(String... lines) {
+        HologramBuilder builder = new ForgeHologramBuilder();
+        builder.lines(lines);
+        return builder;
+    }
+
+    @Override
+    public HologramBuilder builder(String world, int x, int y, int z) {
+        HologramBuilder builder = new ForgeHologramBuilder();
+        builder.position(x, y, z);
+        return builder;
+    }
+}
