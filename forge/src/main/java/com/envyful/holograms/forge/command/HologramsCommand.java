3472f04984473ac8a2a1440ba345e8a079f79749
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-17 07:04:59 +0100

    Feat: added Holograms command

---
+++ forge/src/main/java/com/envyful/holograms/forge/command/HologramsCommand.java
@@ -0,0 +1,28 @@
+package com.envyful.holograms.forge.command;
+
+import com.envyful.api.command.annotate.Command;
+import com.envyful.api.command.annotate.Permissible;
+import com.envyful.api.command.annotate.SubCommands;
+import com.envyful.api.command.annotate.executor.CommandProcessor;
+import com.envyful.api.command.annotate.executor.Sender;
+import net.minecraft.command.ICommandSender;
+
+@Command(
+        value = "holograms",
+        description = "Main holograms command",
+        aliases = {
+                "hd",
+                "holographicdisplays"
+        }
+)
+@Permissible("holograms.command")
+@SubCommands(HologramsCreateCommand.class)
+public class HologramsCommand {
+
+    @CommandProcessor
+    public void helpCommand(@Sender ICommandSender sender, String[] args) {
+        //TODO: send help command
+    }
+
+
+}
