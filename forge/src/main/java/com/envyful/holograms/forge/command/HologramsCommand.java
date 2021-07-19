9ad5a1ac1ae6c121628dc5d0ab30ce2e5cb3120a
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-19 23:31:51 +0100

    Feat: added help message

--- forge/src/main/java/com/envyful/holograms/forge/command/HologramsCommand.java
+++ forge/src/main/java/com/envyful/holograms/forge/command/HologramsCommand.java
@@ -6,6 +6,7 @@ import com.envyful.api.command.annotate.SubCommands;
 import com.envyful.api.command.annotate.executor.CommandProcessor;
 import com.envyful.api.command.annotate.executor.Sender;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.util.text.TextComponentString;
 
 @Command(
         value = "holograms",
@@ -31,10 +32,20 @@ import net.minecraft.command.ICommandSender;
 })
 public class HologramsCommand {
 
+    private static final String HELP_MESSAGE =
+            "§e/hd create <name> (line) §7 - Creates hologram\n" +
+            "§e/hd delete <name> §7 - Deletes hologram\n" +
+            "§e/hd near <radius> §7 - Finds nearby holograms\n" +
+            "§e/hd tp <hologram> §7 - Teleports to hologram\n" +
+            "§e/hd movehere <hologram> §7 - Teleports hologram to you\n" +
+            "§e/hd addline <hologram> <line> §7 - Adds line to hologram\n" +
+            "§e/hd removeline <hologram> <line#> §7 - Removes line from hologram\n" +
+            "§e/hd setline <hologram> <line#> <line> §7 - Sets line on hologram\n" +
+            "§e/hd insert <hologram> <line#> <line> §7 - Inserts line to hologram\n" +
+            "§e/hd copy <hologram> <newHologram> §7 - Copies a hologram";
+
     @CommandProcessor
     public void helpCommand(@Sender ICommandSender sender, String[] args) {
-        //TODO: send help command
+        sender.sendMessage(new TextComponentString(HELP_MESSAGE));
     }
-
-
 }
