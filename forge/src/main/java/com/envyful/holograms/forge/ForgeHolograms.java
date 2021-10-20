62f1c9c546cfd6b61420f2d1d9f20fa7e017ca67
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-20 11:02:36 +0100

    chore(gradle): bump version

--- forge/src/main/java/com/envyful/holograms/forge/ForgeHolograms.java
+++ forge/src/main/java/com/envyful/holograms/forge/ForgeHolograms.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 )
 public class ForgeHolograms {
 
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.5.0";
 
     private static ForgeHolograms instance;
 
