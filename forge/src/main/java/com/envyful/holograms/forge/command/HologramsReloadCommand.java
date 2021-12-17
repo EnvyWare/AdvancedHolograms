a542225bc12c218f7b3f2b2d5fef5f628fd06865
Daniel Norris <dnorris443@gmail.com>
2021-12-17 10:49:03 +0000

    fix(forge): remove reload requirements

--- forge/src/main/java/com/envyful/holograms/forge/command/HologramsReloadCommand.java
+++ forge/src/main/java/com/envyful/holograms/forge/command/HologramsReloadCommand.java
@@ -3,11 +3,9 @@ package com.envyful.holograms.forge.command;
 import com.envyful.api.command.annotate.Child;
 import com.envyful.api.command.annotate.Command;
 import com.envyful.api.command.annotate.Permissible;
-import com.envyful.api.command.annotate.executor.Argument;
 import com.envyful.api.command.annotate.executor.CommandProcessor;
 import com.envyful.api.command.annotate.executor.Sender;
 import com.envyful.api.forge.chat.UtilChatColour;
-import com.envyful.holograms.api.hologram.Hologram;
 import com.envyful.holograms.forge.ForgeHolograms;
 import com.envyful.holograms.forge.hologram.HologramManager;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -22,7 +20,7 @@ import net.minecraft.util.text.TextComponentString;
 public class HologramsReloadCommand {
 
     @CommandProcessor
-    public void addLineCommand(@Sender EntityPlayerMP sender, @Argument Hologram hologram, @Argument String newId) {
+    public void addLineCommand(@Sender EntityPlayerMP sender) {
         sender.sendMessage(new TextComponentString(UtilChatColour.translateColourCodes(
                 '&',
                 ForgeHolograms.getInstance().getConfig().getReloadingMessage()
