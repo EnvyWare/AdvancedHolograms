f86481a26534c06cecabf57c546e777756b5cc8d
danorris709 <dnorris443@gmail.com>
2021-12-30 18:18:15 +0000

    feat(forge16): add config package

---
+++ forge16/src/main/java/com/envyful/advanced/holograms/forge/config/HologramsConfig.java
@@ -0,0 +1,28 @@
+package com.envyful.advanced.holograms.forge.config;
+
+import com.envyful.api.config.data.ConfigPath;
+import com.envyful.api.config.yaml.AbstractYamlConfig;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+@ConfigPath("config/AdvancedHolograms/config.yml")
+@ConfigSerializable
+public class HologramsConfig extends AbstractYamlConfig {
+
+    private String storageLocation = "config/AdvancedHolograms/holograms.json";
+    private String reloadingMessage = "&e&l(!) &eReloading database...";
+    private String reloadedMessage = "&e&l(!) &eReloaded database";
+
+    public HologramsConfig() {}
+
+    public String getStorageLocation() {
+        return this.storageLocation;
+    }
+
+    public String getReloadingMessage() {
+        return this.reloadingMessage;
+    }
+
+    public String getReloadedMessage() {
+        return this.reloadedMessage;
+    }
+}
