b9e9e309a7ca8d0f860fe3739830664a8b2b8e39
Daniel Norris <dnorris443@gmail.com>
2022-02-06 20:38:04 +0000

    chore(gradle): bump version to 0.6.6

--- forge/src/main/java/com/envyful/holograms/forge/ForgeHolograms.java
+++ forge/src/main/java/com/envyful/holograms/forge/ForgeHolograms.java
@@ -25,7 +25,7 @@ import java.io.IOException;
 )
 public class ForgeHolograms {
 
-    public static final String VERSION = "0.6.5";
+    public static final String VERSION = "0.6.6";
 
     private static ForgeHolograms instance;
 
--- forge16/src/main/java/com/envyful/advanced/holograms/forge/ForgeHolograms.java
+++ forge16/src/main/java/com/envyful/advanced/holograms/forge/ForgeHolograms.java
@@ -27,7 +27,7 @@ import java.io.IOException;
 public class ForgeHolograms {
 
     public static final String MOD_ID = "advancedholograms";
-    public static final String VERSION = "0.6.5";
+    public static final String VERSION = "0.6.6";
 
     private static ForgeHolograms instance;
 
